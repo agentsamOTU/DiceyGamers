ff854e3a3f7c86c3958279a701725c0cf1b59b33
Sam <samuel.canonaco@ontariotechu.net>
2023-03-13 09:28:47 -0400

    Working dice rolling

--- DiceyGamersUnity/Assets/DiceGame.cs
+++ DiceyGamersUnity/Assets/DiceGame.cs
@@ -19,7 +19,7 @@ public class DiceGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Roll());
     }
 
     void LoadOrCreateFiles()
@@ -32,6 +32,7 @@ public class DiceGame : MonoBehaviour
             string temp = sr.ReadToEnd();
             die1 = JsonUtility.FromJson<DiceOdds>(temp);
             sr.Close();
+            die1.ReTotal();
             die1.Print();
         }
         else
@@ -48,6 +49,7 @@ public class DiceGame : MonoBehaviour
             string temp = sr.ReadToEnd();
             die2 = JsonUtility.FromJson<DiceOdds>(temp);
             sr.Close();
+            die2.ReTotal();
             die1.Print();
         }
         else
@@ -59,15 +61,41 @@ public class DiceGame : MonoBehaviour
             sw.Close();
         }
     }
-    public void Roll()
+    public int Roll()
     {
+        int total=0;
+        float random = Random.Range(0.0f, die1.total);
+        float running = 0;
+        for(int i = 0; i<die1.sideOdds.Length;i++)
+        {
+            running += die1.sideOdds[i];
+            if (random < running)
+            {
+                total += i + 1;
+                break;
+            }
+        }
 
+        random = Random.Range(0.0f, die2.total);
+        running = 0;
+        for (int i = 0; i < die2.sideOdds.Length; i++)
+        {
+            running += die2.sideOdds[i];
+            if (random < running)
+            {
+                total += i + 1;
+                break;
+            }
+        }
+
+        return total;
     }
 }
 
 public class DiceOdds
 {
     public float[] sideOdds;
+    public float total=0;
     public DiceOdds()
     {
         sideOdds = new float[]
@@ -75,6 +103,10 @@ public class DiceOdds
             1.0f/6.0f,1.0f/6.0f,1.0f/6.0f,
             1.0f/6.0f,1.0f/6.0f,1.0f/6.0f
         };
+        foreach(float f in sideOdds)
+        {
+            total+= f;
+        }
     }
     public DiceOdds(float[] sideOdds)
     {
@@ -85,6 +117,14 @@ public class DiceOdds
         for(int i = 0; i<sideOdds.Length;i++)
         {
             Debug.Log("Side " + i + " Odds : " + sideOdds[i]);
+        }
+    }
+    public void ReTotal()
+    {
+        total = 0;
+        foreach(float f in sideOdds)
+        {
+            total += f;
         }
     }
 }
